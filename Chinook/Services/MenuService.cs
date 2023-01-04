5a78317d64dd5f14be3dceda3d3cd63de48584ba
Asela Liyanage <aselal@99x.io>
2023-01-04 11:43:04 +0530

    Feature01 : Created services to retrieve and add data to the database

---
+++ Chinook/Services/MenuService.cs
@@ -0,0 +1,9 @@
+﻿namespace Chinook.Services
+{
+    public class NavMenuStateService
+    {
+        public event Action<long, string> OnChange;
+
+        public void MenuChanged(long newPlaylistId, string newPlaylistName) => OnChange?.Invoke(newPlaylistId, newPlaylistName);
+    }
+}
